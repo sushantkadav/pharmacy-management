483c6c6061dabd8ff9e59b088e00fca138496269
sushant.kadav <sushant.kadav@bizotics.com>
2020-10-02 14:32:45 +0530

    master
    -     user updated

--- src/main/java/com/fluxinfotech/pharmacymanagementsystem/user/UserDTO.java
+++ src/main/java/com/fluxinfotech/pharmacymanagementsystem/user/UserDTO.java
@@ -1,4 +1,21 @@
 package com.fluxinfotech.pharmacymanagementsystem.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String mobileNo;
+    private Boolean isActive;
 }
